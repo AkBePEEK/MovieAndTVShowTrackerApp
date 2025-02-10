55d255e0b0cb8ce486deb104ebd3b99c84c47b6f
AkBe <154293625+AkBePEEK@users.noreply.github.com>
2025-02-10 21:27:17 +0500

    fix: Fixed issue with running the app

---
+++ app/src/main/java/com/example/movietracker/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.movietracker.ui  // Ensure it matches the manifest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
